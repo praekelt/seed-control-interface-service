90c111703258dbb5065fdea5f884b845e9ba0644
TrevorOctober <trevor@praekeltconsulting.com>
2016-11-17 11:58:23 +0200

    add router.register definition

--- dashboards/urls.py
+++ dashboards/urls.py
@@ -5,6 +5,7 @@ from . import views
 router = routers.DefaultRouter()
 router.register(r'dashboard', views.DashboardViewSet)
 router.register(r'userdashboard', views.UserDashboardViewSet)
+router.register(r'definition', views.DefinitionViewSet)
 
 # Wire up our API using automatic URL routing.
 urlpatterns = [
