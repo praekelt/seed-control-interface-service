79a28af0b3a24f5de75540cfa4ee273b68a88f58
Jason Paidoussi <jason@paidoussi.net>
2017-01-05 22:05:11 +0200

    Fixed version lookup

--- services/templatetags/seed.py
+++ services/templatetags/seed.py
@@ -1,10 +1,10 @@
 from django import template
 
-import seed_stage_based_messaging
+import seed_control_interface_service
 
 register = template.Library()
 
 
 @register.simple_tag
 def current_version():
-    return seed_stage_based_messaging.__version__
+    return seed_control_interface_service.__version__
