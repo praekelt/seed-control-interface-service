2326c9617f17f3d4268c13d2ad326a4a7eedbe36
Jason Paidoussi <jason@paidoussi.net>
2017-01-05 21:43:40 +0200

    Prep for initial release

--- seed_control_interface_service/settings.py
+++ seed_control_interface_service/settings.py
@@ -29,7 +29,6 @@ DEBUG = os.environ.get('DEBUG', False)
 
 ALLOWED_HOSTS = ['*']
 
-
 # Application definition
 
 INSTALLED_APPS = (
@@ -107,9 +106,23 @@ STATICFILES_FINDERS = (
 STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
 
-# TEMPLATE_CONTEXT_PROCESSORS = (
-#     "django.core.context_processors.request",
-# )
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates')
+        ],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
 
 # Sentry configuration
 RAVEN_CONFIG = {
